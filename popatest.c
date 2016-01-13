1ef689d7cae5d0c084b989aae93bed8c4bd094c3
Parham Alvani <parham.alvani@gmail.com>
2016-01-13 22:46:58 +0330

    File header was corrected.

--- popatest.c
+++ popatest.c
@@ -1,15 +1,17 @@
 /*
  * In The Name Of God
  * ========================================
- * [] File Name : popatest.c
+ * [] File Name : checkpoint.c
  *
  * [] Creation Date : 13-01-2016
+ *
+ * [] Created By : Pooya Parsa (pooya@pi0.ir)
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
 */
 /*
- * Copyright (c) 2016 Parham Alvani.
+ * Copyright (c) 2016 Parham Alvani and Pooya Parsa.
 */
 #include "proc.c"
 #include "ulib.c"
