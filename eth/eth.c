cb5b003e94a5090ed66bafb9251ad4d7236d086f
pi0 <pyapar@gmail.com>
2016-01-16 03:24:44 +0330

    Network card detected with bochs

--- eth/eth.c
+++ eth/eth.c
@@ -9,64 +9,62 @@
 static ne_t ne;
 
 void
-ethintr()
-{
-  ne_interrupt(&ne);
-  return;
+ethintr() {
+    ne_interrupt(&ne);
+    return;
 }
 
 // not used because ioctl() isn't still implemented.
 int
-ethioctl(struct inode* ip, int request, void* p)
-{
-  switch (request) {
-  case ETH_IPC_SETUP:
+ethioctl(struct inode *ip, int request, void *p) {
+    switch (request) {
+        case ETH_IPC_SETUP:
 //    cprintf("%s: ETH_IPC_SETUP isn't still implemented because of no IPC\n", ne.name);
-    break;
-  }
-  return 0;
+            break;
+    }
+    return 0;
 }
 
 int
-ethread(struct inode* ip, char* p, int n)
-{
-  return ne_pio_read(&ne, (uchar*)p, n);
+ethread(struct inode *ip, char *p, int n) {
+    return ne_pio_read(&ne, (uchar *) p, n);
 }
 
 int
-ethwrite(struct inode* ip, char* p, int n)
-{
-  return ne_pio_write(&ne, (uchar*)p, n);
+ethwrite(struct inode *ip, char *p, int n) {
+    return ne_pio_write(&ne, (uchar *) p, n);
 }
 
 void
-ethinit()
-{
-  int i;
-  char name[] = "eth#";
-  //Ethernet Auto-Probing <http://www.tldp.org/LDP/nag/node50.html>
-  int ports[] = { 0xC100, 0x240, 0x280, 0x320, 0x340, 0x360 };
+ethinit() {
+    int i;
+    int f = 0;
+    char name[] = "eth#";
+    //Ethernet Auto-Probing <http://www.tldp.org/LDP/nag/node50.html>
+    int ports[] = {};//{0xC100, 0x240, 0x280, 0x320, 0x340, 0x360};
 
-  devsw[ETHERNET].write = ethwrite;
-  devsw[ETHERNET].read = ethread;
-  devsw[ETHERNET].ioctl = ethioctl;
+    devsw[ETHERNET].write = ethwrite;
+    devsw[ETHERNET].read = ethread;
+    devsw[ETHERNET].ioctl = ethioctl;
 
-  for (i = 0; i < NELEM(ports); ++i) {
-    memset(&ne, 0, sizeof(ne));
-    name[3] = '0' + i;
-    strncpy(ne.name, name, strlen(name)+1);
-    ne.irq = IRQ_ETH;
-    ne.base = ports[i];
-    if (ne_probe(&ne)) {
-      cprintf("Ethernet: Initialize port %d [0x%x].\n", i, ports[i]);
-      ne_init(&ne);
-      picenable(ne.irq);
-      ioapicenable(ne.irq, 0);
-      break;
+    for (i = 0; i < NELEM(ports); ++i) {
+        memset(&ne, 0, sizeof(ne));
+        name[3] = '0' + i;
+        strncpy(ne.name, name, strlen(name) + 1);
+        ne.irq = IRQ_ETH;
+        ne.base = ports[i];
+        if (ne_probe(&ne)) {
+            cprintf("Ethernet: Initialize port %d [0x%x].\n", i, ports[i]);
+            ne_init(&ne);
+            picenable(ne.irq);
+            ioapicenable(ne.irq, 0);
+            f = 1;
+            break;
+        }
     }
-  }
 
-  cprintf("Ethernet: Initialize failed!\n");
-  return;
+    if (!f)
+        cprintf("Ethernet: Initialize failed!\n");
+    return;
 }
 
